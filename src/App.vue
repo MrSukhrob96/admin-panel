40b4190b10a720ae4484001cc1663f664dcf2864
MrSukhrob96 <sukhrobazimov1996@gmail.com>
2021-08-13 09:19:33 +0500

    initialize

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,70 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="wrapper">
+    <el-row :gutter="6">
+      <el-col :span="5">
+        <menu-component class="menu-component" />
+      </el-col>
+      <el-col :span="19">
+        <nav-component />
+        <el-table :data="tableData" stripe style="width: 100%">
+          <el-table-column prop="date" label="Date" width="180">
+          </el-table-column>
+          <el-table-column prop="name" label="Name" width="180">
+          </el-table-column>
+          <el-table-column prop="address" label="Address"> </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
   </div>
-  <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import MenuComponent from "@/components/menu-component.vue";
+import NavComponent from "@/components/nav-component.vue";
+
+export default {
+  name: "App",
+  components: {
+    MenuComponent,
+    NavComponent,
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-03",
+          name: "Tom",
+          address: "No. 189, Grove St, Los Angeles",
+        },
+        {
+          date: "2016-05-02",
+          name: "Tom",
+          address: "No. 189, Grove St, Los Angeles",
+        },
+        {
+          date: "2016-05-04",
+          name: "Tom",
+          address: "No. 189, Grove St, Los Angeles",
+        },
+        {
+          date: "2016-05-01",
+          name: "Tom",
+          address: "No. 189, Grove St, Los Angeles",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  overflow-x: hidden;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.menu-component {
+  height: 100vh;
 }
 </style>
